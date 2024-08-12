ac1354c99e5a47cfdccfd8f9f57d2cdc31f57f19
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-08-12 20:01:25 -0300

    refactoring: reorganizando a estrutura do repositório

--- Pottencial - dotNET Developer/AvaliacaoTestesAutomatizados/obj/Debug/net6.0/AvaliacaoTestesAutomatizados.AssemblyInfo.cs
+++ Pottencial - dotNET Developer/AvaliacaoTestesAutomatizados/obj/Debug/net6.0/AvaliacaoTestesAutomatizados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaliacaoTestesAutomatizados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f916288138d417c58b287a8fcba934f9f18dae81")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaliacaoTestesAutomatizados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaliacaoTestesAutomatizados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
