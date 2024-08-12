0be6f12573dc6712c8a20b4fa9b2c83f381a7f49
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-08-12 20:22:23 -0300

    feat: entregando desafio de codigo juros compostos

---
+++ XP - Full Stack Developer/Desafios de Código/Calculando-Juros-Compostos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Solicita ao usuário para inserir os valores
+        double P = Convert.ToDouble(Console.ReadLine()); // Lê o valor principal P
+        double i = Convert.ToDouble(Console.ReadLine()); // Lê a taxa de juros i
+        int n = Convert.ToInt32(Console.ReadLine()); // Lê o número de anos n
+
+        // Calcula o montante final utilizando a função CalcularJurosCompostos
+        double montanteFinal = CalcularJurosCompostos(P, i, n);
+
+        // Exibe o resultado
+        Console.WriteLine(montanteFinal); // Mostra o montante final calculado
+    }
+
+    public static double CalcularJurosCompostos(double P, double i, int n)
+    {
+        // Calcula o montante final com juros compostos
+        double montanteFinal = P * Math.Pow(1 + i, n);
+        return montanteFinal;
+    }
+}
